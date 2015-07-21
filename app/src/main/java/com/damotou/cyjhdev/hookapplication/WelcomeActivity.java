47e3021aeb494f51d5e74cbee1988be25b5df5de
damotou <377296294@qq.com>
2015-07-21 22:17:52 +0800

    加入网络日志

--- app/src/main/java/com/damotou/cyjhdev/hookapplication/WelcomeActivity.java
+++ app/src/main/java/com/damotou/cyjhdev/hookapplication/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.damotou.cyjhdev.hookapplication;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -11,14 +12,23 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.net.LocalServerSocket;
+import android.net.LocalSocket;
 import com.damotou.cyjhdev.hookapplication.util.NavUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class WelcomeActivity  extends XhookBaseActivity
 {
     private WelcomeAdapter mAdapter;
+    /* 数据段begin */
+    private final String TAG = "server";
 
+    private ServerSocketThread mServerSocketThread;
+    /* 数据段end */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +37,9 @@ public class WelcomeActivity  extends XhookBaseActivity
 
         setContentView(R.layout.activity_welcome);
 
- //        mAdapter = new WelcomeAdapter(this);
-        // TODO add proper description texts and load them from resources, add icons, make it more fancy, ...
- //       mAdapter.add(new WelcomeItem(R.string.tabInstall, R.string.tabInstallDescription));
-//        mAdapter.add(new WelcomeItem(R.string.tabAbout, R.string.tabAboutDescription));
+        mServerSocketThread = new ServerSocketThread();
+        mServerSocketThread.start();
 
-//        ListView lv = (ListView) findViewById(R.id.welcome_list);
-//        lv.setAdapter(mAdapter);
-//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(WelcomeActivity.this, XHookInstallerActivity.class);
-//                intent.putExtra(XHookInstallerActivity.EXTRA_SECTION, position);
-//                intent.putExtra(NavUtil.FINISH_ON_UP_NAVIGATION, true);
-//                startActivity(intent);
-//                NavUtil.setTransitionSlideEnter(WelcomeActivity.this);
-//            }
-//        });
         Intent intent = new Intent(WelcomeActivity.this, XHookInstallerActivity.class);
         intent.putExtra(XHookInstallerActivity.EXTRA_SECTION, 0);
         intent.putExtra(NavUtil.FINISH_ON_UP_NAVIGATION, true);
@@ -108,6 +104,126 @@ public class WelcomeActivity  extends XhookBaseActivity
         @Override
         public String toString() {
             return title;
+        }
+    }
+    /* 内部类begin */
+    private class ServerSocketThread extends Thread
+    {
+        private boolean keepRunning = true;
+        private LocalServerSocket serverSocket;
+
+        private void stopRun()
+        {
+            keepRunning = false;
+        }
+
+        @Override
+        public void run()
+        {
+            try
+            {
+                serverSocket = new LocalServerSocket("elf_local_socket");
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+
+                keepRunning = false;
+            }
+
+            while(keepRunning)
+            {
+                Log.d(TAG, "wait for new client coming !");
+
+                try
+                {
+                    LocalSocket interactClientSocket = serverSocket.accept();
+
+                    //由于accept()在阻塞时，可能Activity已经finish掉了，所以再次检查keepRunning
+                    if (keepRunning)
+                    {
+                        Log.d(TAG, "new client coming !");
+
+                        new InteractClientSocketThread(interactClientSocket).start();
+                    }
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+
+                    keepRunning = false;
+                }
+            }
+
+            if (serverSocket != null)
+            {
+                try
+                {
+                    serverSocket.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class InteractClientSocketThread extends Thread
+    {
+        private LocalSocket interactClientSocket;
+
+        public InteractClientSocketThread(LocalSocket interactClientSocket)
+        {
+            this.interactClientSocket = interactClientSocket;
+        }
+
+        @Override
+        public void run()
+        {
+            StringBuilder recvStrBuilder = new StringBuilder();
+            InputStream inputStream = null;
+            try
+            {
+                inputStream = interactClientSocket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                char[] buf = new char[4096];
+                int readBytes = -1;
+                while ((readBytes = inputStreamReader.read(buf)) != -1)
+                {
+                    String tempStr = new String(buf, 0, readBytes);
+                    recvStrBuilder.append(tempStr);
+                    String recvString = recvStrBuilder.toString();
+                    if(recvString.contains("NULL"))
+                    {
+                        Log.d(TAG,"FAILED");
+                    }else if(recvString.contains("SUCCESS"))
+                    {
+                        Log.d(TAG,"SUCCESS");
+                    }
+                    Log.d(TAG,recvStrBuilder.toString());
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+
+                Log.d(TAG, "resolve data error !");
+            }
+            finally
+            {
+                if (inputStream != null)
+                {
+                    try
+                    {
+                        inputStream.close();
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     }
 }
